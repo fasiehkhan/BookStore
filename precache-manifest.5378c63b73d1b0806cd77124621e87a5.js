939c234359aeadef72f0f1310b4272b836d3fece
fasiehkhan <fasiehkhan2@gmail.com>
2020-02-19 17:12:43 -0500

    Updates

---
+++ precache-manifest.5378c63b73d1b0806cd77124621e87a5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc72643ec948caf40468f3b57dd7a47c",
+    "url": "/BookStore/index.html"
+  },
+  {
+    "revision": "bcc75d65d73625291ddc",
+    "url": "/BookStore/static/css/main.46188b86.chunk.css"
+  },
+  {
+    "revision": "c024943a01c7589cdb05",
+    "url": "/BookStore/static/js/2.1fb45afe.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/BookStore/static/js/2.1fb45afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc75d65d73625291ddc",
+    "url": "/BookStore/static/js/main.b9863c09.chunk.js"
+  },
+  {
+    "revision": "f152a2010bd7b414eaf0",
+    "url": "/BookStore/static/js/runtime-main.747b0e28.js"
+  },
+  {
+    "revision": "d33edf89641fd987b0f267b1f9453155",
+    "url": "/BookStore/static/media/book1.d33edf89.png"
+  },
+  {
+    "revision": "2b8f53f9288e8721f9189ceadcf65764",
+    "url": "/BookStore/static/media/book11.2b8f53f9.jpg"
+  },
+  {
+    "revision": "9c6290e14167fb47c260a04cd52bf38c",
+    "url": "/BookStore/static/media/book2.9c6290e1.jpg"
+  },
+  {
+    "revision": "6131a7e7f0cdadda476be21507f667b1",
+    "url": "/BookStore/static/media/book20.6131a7e7.png"
+  },
+  {
+    "revision": "1140ba099cdbab368ea1ad9a028cf3cf",
+    "url": "/BookStore/static/media/book21.1140ba09.png"
+  },
+  {
+    "revision": "346a4c8ba27a1092a4693ddbfd984776",
+    "url": "/BookStore/static/media/book22.346a4c8b.png"
+  },
+  {
+    "revision": "126b685c4dd298b2731c90e000a687e5",
+    "url": "/BookStore/static/media/book4.126b685c.jpg"
+  },
+  {
+    "revision": "9a111c1828a563c7aaa9a55e0ba17670",
+    "url": "/BookStore/static/media/book6.9a111c18.jpg"
+  },
+  {
+    "revision": "f446e71702c8c8c06abc0070694a2fca",
+    "url": "/BookStore/static/media/book7.f446e717.jpg"
+  },
+  {
+    "revision": "afa7cbfa26dd0f06e01fc7eb292bbddf",
+    "url": "/BookStore/static/media/nR1.afa7cbfa.png"
+  },
+  {
+    "revision": "701b5456472f66dafe1d73345b52d7f3",
+    "url": "/BookStore/static/media/nR2.701b5456.png"
+  },
+  {
+    "revision": "e920f5041ca8b613f76a72b400dd41a9",
+    "url": "/BookStore/static/media/nR3.e920f504.png"
+  }
+]);
